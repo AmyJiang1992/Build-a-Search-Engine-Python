1dac8b671f3b790460f9c1c9b0a885715c7d9fac
AmyJiang1992 <jiangruotong2013@gmail.com>
2016-07-27 19:33:20 -0400

    Update buildaSearchEngine_1.py

--- buildaSearchEngine_1.py
+++ buildaSearchEngine_1.py
@@ -67,9 +67,91 @@ def union(p,q):
 # Now we are ready to finish the web crawler.
 # What a  web crawler do is: we have a seed page with some links on it. We want to be able to find those links.
 # We get them to list and then we follow them to new pages. New pages might also have links and we want to follow those links.
-# In order to do this, we need to think about to things.
+# In order to do this, we need to think about two things.
 # First, keep track of all pages that we're waiting to crawl
 
-# @tocrawl: a list of pages left to crawl. Initially, the seed page
+# @tocrawl: a list of pages left to crawl. Initially, the seed page.
+# @crawled: a list of pages crawled. Initially, empty list, [].
+
+# pseudo code:
+start with tocrawl = [seed]
+crawled = []
+while there are more pages to crawl:
+    pick a page from tocrawl
+    add that page to crawled
+    add all the link targets on this page to tocrawl
+return crawled
+
+# HOWEVER, if we follow this process on the given test site, with the seed page, it will never return.
+# The crawler will never finish because it will always find a link to crawl.
+# Example: page A has link to page B, and page B has link to page A, the two pages links to each other again and again.
+# TO avoid this, we need to do something smarter. We need to make sure that we don't crawl pages that we already done.
+# update pseudo code:
+start with tocrawl = [seed]
+crawled = []
+while there are more pages to crawl:
+    pick a page from tocrawl
+    check if we already crawled it!
+    add that page to crawled
+    add all the link targets on this page to tocrawl
+return crawled    
+# Now we are going to define a procedure crawl_web that takes as input a seed page url, and 
+# output a list of all urls that can be reached by following links starting from the seed page.
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            union(tocrawl, get_all_links(get_page(page)))
+            crawled.append(page)
+    return crawled
+# Procedure get_page is a given procedure transfer url to html. A procudure which actually grabs a page from the web
+# will be talked about later.
+
+# The web crawler we built has some serious flaws if we were going to use it in a real crawler. 
+# One problem is if we start with a good seed page, it might run for an extremely long time (even forever, since the
+# number of URLS on the web is not actually finite). 
+# Now we are going to explore two different ways to limit the pages that it can crawl.
+
+# The first way:
+# Modify the crawl_web procedure to take a second parameter, max_pages, that limits the number of pages to crawl.
+# We expect the procedure terminate the crawl after max_pages different pages have been crawled,
+# or when there are no more pages to crawl.
+# Here is updated crawl_web procedure:
+def crawl_web(seed, max_pages):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled and len(crawled) < max_pages:
+            union(tocrawl, get_all_links(get_page(page)))
+            crawled.append(page)
+    return crawled
+# The second way:
+# Modify the crawl_web procedure to take a second parameter, max_depth, that limits the depth of the search.  
+# We can define the depth of a page as the number of links that must be followed to reach that page starting from the seed page,
+# that is, the length of the shortest path from the seed to the page.  
+# No pages whose depth exceeds max_depth should be included in the crawl.  
+# For example
+# If max_depth is 0, the only page that should be crawled is the seed page. 
+# If max_depth is 1, the pages that should be crawled are the seed page and every page that it links to directly. 
+# If max_depth is 2, the crawl should also include all pages that are linked to by these pages.
+# Here is updated crawl_web procedure:
+def crawl_web(seed,max_depth):    
+    tocrawl = [seed]
+    crawled = []
+    next_depth = []# Keep track of next level of links
+    depth = 0
+    while tocrawl and depth <= max_depth:
+        page = tocrawl.pop()
+        if page not in crawled:
+            union(next_depth, get_all_links(get_page(page)))
+            crawled.append(page)
+        if not tocrawl:
+        # When tocrawl is empty, it means this level is all crawled and ready to craw next level
+            tocrawl, next_depth = next_depth, []
+            depth = depth + 1
+    return crawled
 
 
